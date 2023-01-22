5f1477bdf4b63428e568c79509906a6ddd6eba76
Ranjit kumar suryawanshi <djdj775257@gmail.com>
2023-01-22 19:05:39 +0530

    first fix ui 5.0

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 // import logo from './logo.svg';
-// import EmailComp from './container/EmailComp'
+import EmailComp from './container/EmailComp'
 // import EmailComp2 from './container/EmailComp2'
-// import EmailTwo from './container/EmailTwo'
-// import CalendarComp from './container/CalendarComp'
-// import CalendarComp2 from './container/CalendarComp2'
-// import CalendarComp2One from './container/CalendarComp2One'
-// import WorkSpaceCS from './container/WorkSpaceCS'
+import EmailTwo from './container/EmailTwo'
+import CalendarComp from './container/CalendarComp'
+import CalendarComp2 from './container/CalendarComp2'
+import CalendarComp2One from './container/CalendarComp2One'
+import WorkSpaceCS from './container/WorkSpaceCS'
 import CalendarComp3 from './container/CalendarComp3'
 import VideoCenterOne from './container/VideoCenterOne'
 import './App.css';
@@ -13,13 +13,13 @@ import './App.css';
 function App() {
   return (
     <div className='App'>
-      {/* <EmailComp /> */}
+      <EmailComp />
       {/* <EmailComp2 /> */}
-      {/* <EmailTwo /> */}
-      {/* <CalendarComp /> */}
-      {/* <CalendarComp2 /> */}
-      {/* <CalendarComp2One /> */}
-      {/* <WorkSpaceCS /> */}
+      <EmailTwo />
+      <CalendarComp />
+      <CalendarComp2 />
+      <CalendarComp2One />
+      <WorkSpaceCS />
       <CalendarComp3 />
       <VideoCenterOne />
     </div>
